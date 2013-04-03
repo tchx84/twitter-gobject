303752d45a5cea323c0661b1e24ac3f8ee1cc38e
Martin Abente Lahaye <tch@sugarlabs.org>
2013-04-02 22:25:18 -0400

    Fix LGPL header and PEP8
    Add LGPL header in all source files.
    
    Fix more PEP8 compliance errors.

--- tests/test_twr_search.py
+++ tests/test_twr_search.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 #
 # Copyright (c) 2013 Martin Abente Lahaye. - tch@sugarlabs.org
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
 
 import sys
 
@@ -52,7 +49,7 @@ def __phase1_cb(search, info):
 search = TwrSearch()
 search.connect('tweets-downloaded', __phase1_cb)
 search.connect('tweets-downloaded-failed', __phase1_failed_cb)
-search.tweets('@tchx84', count=1)
+search.tweets('@twr_object', count=1)
 
 loop = GObject.MainLoop()
 loop.run()
--- tests/test_twr_status.py
+++ tests/test_twr_status.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 #
 # Copyright (c) 2013 Martin Abente Lahaye. - tch@sugarlabs.org
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
 
 import sys
 import random
@@ -88,24 +85,24 @@ def __test_phase6_cb(status, info):
 
 
 def __test_phase5_cb(parent_status, info):
-    print '[FAILED] phase5: retweet-created, _status_id: %s' %\
-            parent_status._status_id
+    print '[FAILED] phase5: retweet-created, _status_id: %s' % \
+          parent_status._status_id
     loop.quit()
 
 
 def __test_phase4_cb(parent_status, info, child_status):
-    print '[OK] phase4: status-downloaded, _status_id: %s' %\
-            parent_status._status_id
+    print '[OK] phase4: status-downloaded, _status_id: %s' % \
+          parent_status._status_id
 
     parent_status.connect('retweet-created', __test_phase5_cb)
     parent_status.connect('retweet-created-failed',
-                            __test_phase5_failed_cb, child_status)
+                          __test_phase5_failed_cb, child_status)
     parent_status.retweet()
 
 
 def __test_phase3_cb(child_status, info, parent_status):
-    print '[OK] phase3: status-downloaded, _status_id: %s' %\
-            child_status._status_id
+    print '[OK] phase3: status-downloaded, _status_id: %s' % \
+          child_status._status_id
 
     parent_status.connect('status-downloaded', __test_phase4_cb, child_status)
     parent_status.connect('status-downloaded-failed', __test_phase4_failed_cb)
@@ -113,8 +110,8 @@ def __test_phase3_cb(child_status, info, parent_status):
 
 
 def __test_phase2_cb(child_status, info, parent_status):
-    print '[OK] phase2: status-updated (reply), _status_id: %s' %\
-            child_status._status_id
+    print '[OK] phase2: status-updated (reply), _status_id: %s' % \
+          child_status._status_id
 
     child_status.connect('status-downloaded', __test_phase3_cb, parent_status)
     child_status.connect('status-downloaded-failed', __test_phase3_failed_cb)
@@ -122,8 +119,8 @@ def __test_phase2_cb(child_status, info, parent_status):
 
 
 def __test_phase1_cb(parent_status, info):
-    print '[OK] phase1: status-updated, _status_id: %s' %\
-            parent_status._status_id
+    print '[OK] phase1: status-updated, _status_id: %s' % \
+          parent_status._status_id
 
     parent_name = str(info['user']['name'])
     status = '@%s %s' % (parent_name, random_status())
